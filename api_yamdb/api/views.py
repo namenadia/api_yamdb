25c36e135d544fe7cddd942c9a983a20d69a87d8
Nadia Kotukhina <kotukhinidea@yandex.ru>
2024-01-20 19:54:20 +0300

    Add permission_classes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
-from rest_framework.pagination import LimitOffsetPagination
 
 from reviews.models import Category, Genre, Title
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+# from .permissions import IsAdminOrReadOnly
 
 
 class ListCreateDestroyViewSet(
@@ -12,7 +12,9 @@ class ListCreateDestroyViewSet(
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet
 ):
-    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+#   permission_classes = [IsAdminOrReadOnly,]
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -21,21 +23,14 @@ class TitleViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('categories__slug', 'geners__slug', 'name', 'year')
     http_method_names = ['get', 'post', 'patch', 'delete']
-    pagination_class = LimitOffsetPagination
-#   permission_classes = ...
+#   permission_classes = [IsAdminOrReadOnly,]
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name',)
-#   permission_classes = ...
 
 
 class GenreViewSet(ListCreateDestroyViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name',)
-#   permission_classes = ...
