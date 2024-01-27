f38e522a6d51d2d6673737dcb7fcc974d20f07cc
Nadia <142178538+nyakot@users.noreply.github.com>
2024-01-27 11:59:56 +0300

    Update current_year

--- api_yamdb/reviews/utils.py
+++ api_yamdb/reviews/utils.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 
-def validate_year(value):
+def current_year(value):
     now = timezone.now().year
     if value > now:
         raise ValidationError(
